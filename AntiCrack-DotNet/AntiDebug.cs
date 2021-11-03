da1393ded3a76e7b2ce9f92fe019a52fe913aa5e
AhmedMinegames <90452585+AhmedMinegames@users.noreply.github.com>
2021-11-03 15:54:36 -0700

    Add files via upload

---
+++ AntiCrack-DotNet/AntiDebug.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace AntiCrack_DotNet
+{
+    class AntiDebug
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr Handle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool IsDebuggerPresent();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CheckRemoteDebuggerPresent(IntPtr Handle, ref bool CheckBool);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lib);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr ModuleHandle, string Function);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr ProcHandle, IntPtr BaseAddress, byte[] Buffer, uint size, int NumOfBytes);
+
+        [DllImport("ntdll.dll")]
+        private static extern uint NtSetInformationThread(IntPtr ThreadHandle, uint ThreadInformationClass, IntPtr ThreadInformation, int ThreadInformationLength);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenThread(uint DesiredAccess, bool InheritHandle, int ThreadId);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint GetTickCount();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern void OutputDebugStringA(string Text);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern uint GetLastError();
+
+        public static bool CloseHandleAntiDebug()
+        {
+            try
+            {
+                CloseHandle((IntPtr)0x1231222L);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "External component has thrown an exception.")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool DebuggerIsAttached()
+        {
+            if (Debugger.IsAttached)
+                return true;
+            return false;
+        }
+
+        public static bool IsDebuggerPresentCheck()
+        {
+            if (IsDebuggerPresent())
+                return true;
+            return false;
+        }
+
+        public static bool CheckRemoteDebuggerPresentCheck()
+        {
+            bool IsThereADebuggerPresent = false;
+            CheckRemoteDebuggerPresent(Process.GetCurrentProcess().Handle, ref IsThereADebuggerPresent);
+            if (IsThereADebuggerPresent)
+                return true;
+            return false;
+        }
+
+        public static string PatchingDbgUiRemoteBreakin()
+        {
+            IntPtr NtdllModule = GetModuleHandle("ntdll.dll");
+            IntPtr DbgUiRemoteBreakinAddress = GetProcAddress(NtdllModule, "DbgUiRemoteBreakin");
+            byte[] Int3InvaildCode = { 0xCC };
+            bool Status = WriteProcessMemory(Process.GetCurrentProcess().Handle, DbgUiRemoteBreakinAddress, Int3InvaildCode, 1, 0);
+            if (Status)
+                return "Success"; 
+            return "Failed";
+        }
+
+        public static bool FindWindowAntiDebug()
+        {
+            Process[] GetProcesses = Process.GetProcesses();
+            foreach(Process GetWindow in GetProcesses)
+            {
+                string[] BadWindowNames = { "x32dbg", "x64dbg", "ollydbg" };
+                foreach (string BadWindows in BadWindowNames)
+                {
+                    if (GetWindow.MainWindowTitle.ToLower().Contains(BadWindows))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static string HideThreadsAntiDebug()
+        {
+            ProcessThreadCollection GetCurrentProcessThreads = Process.GetCurrentProcess().Threads;
+            foreach (ProcessThread Threads in GetCurrentProcessThreads)
+            {
+                IntPtr ThreadsHandle = OpenThread(0x0020, false, Threads.Id);
+                if (ThreadsHandle != IntPtr.Zero)
+                {
+                    uint Status = NtSetInformationThread(ThreadsHandle, 0x11, IntPtr.Zero, 0);
+                    if (Status == 0x00000000)
+                        return "Success";
+                }
+            }
+            return "Failed";
+        }
+
+        public static bool GetTickCountAntiDebug()
+        {
+            uint Start = GetTickCount();
+            string AnyJob = Process.GetCurrentProcess().MainModule.FileName;
+            string AnyJob2 = Process.GetCurrentProcess().PrivateMemorySize64.ToString();
+            return (GetTickCount() - Start) > 2;
+        }
+
+        public static bool OutputDebugStringAntiDebug()
+        {
+            OutputDebugStringA("just testing some stuff...");
+            if (GetLastError() == 0)
+                return true;
+            return false;
+        }
+
+        public static void OllyDbgFormatStringExploit()
+        {
+            OutputDebugStringA("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s");
+        }
+
+        public static bool DebugBreakAntiDebug()
+        {
+            try
+            {
+                Debugger.Break();
+                return false;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ AntiCrack-DotNet/AntiVirtualization.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AntiCrack_DotNet
+{
+    class AntiVirtualization
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lib);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr ModuleHandle, string Function);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr ProcHandle, IntPtr BaseAddress, byte[] Buffer, uint size, int NumOfBytes);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool IsProcessCritical(IntPtr Handle, ref bool BoolToCheck);
+
+        public static string IsSandboxiePresent()
+        {
+            if (GetModuleHandle("SbieDll.dll").ToInt32() != 0)
+                return "[Bad]";
+            return "[Good]";
+        }
+
+        public static string IsComodoSandboxPresent()
+        {
+            if (GetModuleHandle("cmdvrt32.dll").ToInt32() != 0 || GetModuleHandle("cmdvrt64.dll").ToInt32() != 0)
+                return "[Bad]";
+            return "[Good]";
+        }
+
+        public static string IsQihoo360SandboxPresent()
+        {
+            if (GetModuleHandle("SxIn.dll").ToInt32() != 0)
+                return "[Bad]";
+            return "[Good]";
+        }
+
+        public static string IsCuckooSandboxPresent()
+        {
+            if (GetModuleHandle("cuckoomon.dll").ToInt32() != 0)
+                return "[Bad]";
+            return "[Good]";
+        }
+
+        public static string IsEmulationPresent()
+        {
+            long Tick = Environment.TickCount;
+            Thread.Sleep(500);
+            long Tick2 = Environment.TickCount;
+            if (((Tick2 - Tick) < 500L))
+            {
+                return "[Bad]";
+            }
+            return "[Good]";
+        }
+
+        public static string IsWinePresent()
+        {
+            IntPtr ModuleHandle = GetModuleHandle("kernel32.dll");
+            if (GetProcAddress(ModuleHandle, "wine_get_unix_file_name").ToInt32() != 0)
+                return "[Bad]";
+            return "[Good]";
+        }
+
+        public static string CheckForVMwareAndVirtualBox()
+        {
+            using (ManagementObjectSearcher ObjectSearcher = new ManagementObjectSearcher("Select * from Win32_ComputerSystem"))
+            {
+                using (ManagementObjectCollection ObjectItems = ObjectSearcher.Get())
+                {
+                    foreach (ManagementBaseObject Item in ObjectItems)
+                    {
+                        string ManufacturerString = Item["Manufacturer"].ToString().ToLower();
+                        string ModelName = Item["Model"].ToString();
+                        if ((ManufacturerString == "microsoft corporation" && ModelName.ToUpperInvariant().Contains("VIRTUAL") || ManufacturerString.Contains("vmware")))
+                        {
+                            return "[Bad]";
+                        }
+                    }
+                }
+            }
+            return "[Good]";
+        }
+
+        public static string CheckForKVM()
+        {
+            string[] BadDriversList = { "balloon.sys", "netkvm.sys", "vioinput", "viofs.sys", "vioser.sys" };
+            string[] DriversFiles = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\System32", "*");
+            foreach (string Drivers in DriversFiles)
+            {
+                foreach(string BadDrivers in BadDriversList)
+                {
+                    if (Drivers.Contains(BadDrivers))
+                    {
+                        return "[Bad]";
+                    }
+                }
+            }
+            return "[Good]";
+        }
+
+        public static string CheckForHyperV()
+        {
+            ServiceController[] GetServicesOnSystem = ServiceController.GetServices();
+            foreach (ServiceController CompareServicesNames in GetServicesOnSystem)
+            {
+                string[] Services = { "vmbus", "VMBusHID", "hyperkbd" };
+                foreach (string ServicesToCheck in Services)
+                {
+                    if (CompareServicesNames.ServiceName.Contains(ServicesToCheck))
+                        return "[Bad]";
+                }
+            }
+            return "[Good]";
+        }
+        
+        public static string CheckForBlacklistedNames()
+        {
+            string[] BadNames = { "Johnson", "Miller", "malware", "maltest", "CurrentUser", "Sandbox", "virus", "John Doe", "test user", "sand box" };
+            foreach (string BadUsernames in BadNames)
+            {
+                if (Environment.UserName == BadUsernames)
+                {
+                    return "[Bad]";
+                }
+            }
+            return "[Good]";
+        }
+
+        public static void CrashingSandboxie()
+        {
+            byte[] UnHookedCode = { 0xB8, 0x26, 0x00, 0x00, 0x00 };
+            IntPtr NtdllModule = GetModuleHandle("ntdll.dll");
+            IntPtr NtOpenProcess = GetProcAddress(NtdllModule, "NtOpenProcess");
+            WriteProcessMemory(Process.GetCurrentProcess().Handle, NtOpenProcess, UnHookedCode, 5, 0);
+            Process[] GetProcesses = Process.GetProcesses();
+            foreach(Process ProcessesHandle in GetProcesses)
+            {
+                bool DoingSomethingWithHandle = false;
+                try
+                {
+                    IsProcessCritical(ProcessesHandle.Handle, ref DoingSomethingWithHandle);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
