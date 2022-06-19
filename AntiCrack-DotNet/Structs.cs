ade360f89d3085621480239df6e21274b3c1a38e
AdvDebug <90452585+AdvDebug@users.noreply.github.com>
2022-06-18 18:05:30 -0700

    Add files via upload

---
+++ AntiCrack-DotNet/Structs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiCrack_DotNet
+{
+    class Structs
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CONTEXT
+        {
+            public long ContextFlags;
+            public uint Dr0;
+            public uint Dr1;
+            public uint Dr2;
+            public uint Dr3;
+            public uint Dr4;
+            public uint Dr5;
+            public uint Dr6;
+            public uint Dr7;
+        }
+
+        public struct PROCESS_MITIGATION_BINARY_SIGNATURE_POLICY
+        {
+            public uint MicrosoftSignedOnly;
+        }
+    }
+}
